e11a0deeba41bcd86a2ac11dd779029a634ac19a
AfiomaComplate <322639251+AfiomaComplate@users.noreply.github.com>
2026-09-09 17:24:22 +0000

    Point Mini App support at live Render bot

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 window.DATA = {
   site: "https://ggsel.net/sellers",
-  botApi: "",
+  botApi: "https://ggsell.onrender.com",
   currencies: [
     { id: "RUB", label: "RUB", name: "Рубль", mark: "₽" },
     { id: "UAH", label: "UAH", name: "Гривна", mark: "₴" },
